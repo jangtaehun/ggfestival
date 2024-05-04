ec260e5cb92813b862256353eb41e39d4b7eb117
장태훈 <jangtaehun@jangtaehuns-MacBook-Pro.local>
2024-05-04 23:10:48 +0900

    init

---
+++ ggtour.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+
+
+# file = open(f"경기도문화행사현황.csv", "r", encoding="CP949")
+# line = file.readline()
+# print(line)
+
+
