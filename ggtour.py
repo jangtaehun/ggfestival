b12212a7d317886be8a43c3fcac8565fdf30e88b
장태훈 <jangtaehun@jangtaehuns-MacBook-Pro.local>
2024-05-05 23:56:31 +0900

    init

--- ggtour.py
+++ ggtour.py
@@ -1,11 +1,18 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+import requests
+from bs4 import BeautifulSoup
+from playwright.sync_api import sync_playwright
 import csv
+import time
 
+p = sync_playwright().start()
+browser = p.chromium.launch(headless=False)  # 기본값 True: 웹 안 뜸
+page = browser.new_page()
 
-# file = open(f"경기도문화행사현황.csv", "r", encoding="CP949")
-# line = file.readline()
-# print(line)
+jobs_db = []
 
-
+page.goto("http://www.ggac.or.kr/?p=14")
+time.sleep(2)
+page.click("div.month>ul>li")
+time.sleep(2)
+page.click("div.photo_list>ul>li")
+time.sleep(2)
